26192d9b9beb87800d78e2492bc3835a3f48c2c4
Fabio Lima <fabiolimace@gmail.com>
2018-11-03 16:26:28 -0300

    Added a popup example

---
+++ Example/Popup Example/Resources/CustomScript.js
@@ -0,0 +1,57 @@
+
+var current = 0;
+
+var CLASS_PAGE = "Page";
+
+function find(id) {
+	return document.getElementById(id);
+}
+
+function findAll() {
+	return document.getElementsByClassName(CLASS_PAGE);
+}
+
+function show(id) {
+	find(id).style.display = "block";
+}
+
+function hideAll() {
+	var pages = findAll(CLASS_PAGE);
+	for (var i = 0; i < pages.length; i++) {
+		pages[i].style.display = "none";
+	}
+}
+
+function go(id) {
+	setTimeout(function(){
+		hideAll();
+		show(id);
+	}, 300);
+}
+
+function goIndex(i) {
+
+	var pages = findAll();
+	
+	if (i < 0) {
+		current = 0;
+	} else if (i > pages.length - 1) {
+		current = pages.length - 1;
+	} else {
+		current = i;
+	}
+
+	go(pages[current].id);
+}
+
+function first() {
+	goIndex(0);
+}
+
+function next() {
+	goIndex(++current);
+}
+
+function previous() {
+	goIndex(--current);
+}
